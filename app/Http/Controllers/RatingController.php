878a33d3aaa6a810ed66b7c02ad738d0fb9da982
kweziEpsi <kwezi@digilims.com>
2022-06-05 22:06:43 +0200

    Rating post

---
+++ app/Http/Controllers/RatingController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\Rating;
+use Illuminate\Http\Request;
+use App\Http\Requests\RatingRequest;
+use Illuminate\Support\Facades\Auth;
+
+class RatingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(RatingRequest $request, $id)
+    {
+        $post = Post::find($id);
+        $post_id = $post->id;
+
+        $rate = new Rating;
+
+        $rate->user_id = Auth::id();
+        $rate->post_id = $post_id;
+        $rate->rating = $request->input('rating');
+        $rate->save();
+
+        return redirect()->route('home');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
